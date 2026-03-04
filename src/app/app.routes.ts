92a0beb7b10326c6f781181771fac453ea7dbc14
abhishek.kale <abhisahebpatil@gmail.com>
2026-03-05 00:07:58 +0530

    completed-develop-header-section-dropdown-main-binding-inside-other-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    { path: 'data-binding', loadComponent: () => import('./components/basic-data-binding/basic-data-binding').then((c) => c.BasicDataBinding) },
-    { path: 'event-binding', loadComponent: () => import('./components/event-binding/event-binding').then((c) => c.EventBinding) },
-    { path: 'ngModel-NgModelChange', loadComponent: () => import('./components/basic-ng-model-ng-model-change/basic-ng-model-ng-model-change').then((c) => c.BasicNgModelNgModelChange) }
-
+    {path:'',redirectTo:'main-binding',pathMatch:'full'},
+    {
+        path: 'main-binding',
+        loadComponent: () => import('./components/main-data-binding/main-data-binding').then((c) => c.MainDataBinding),
+        children: [
+            { path: '', redirectTo: '', pathMatch: 'full' },
+            { path: 'data-binding', loadComponent: () => import('./components/main-data-binding/basic-data-binding/basic-data-binding').then((c) => c.BasicDataBinding) },
+            { path: 'event-binding', loadComponent: () => import('./components/main-data-binding/event-binding/event-binding').then((c) => c.EventBinding) },
+            { path: 'ngModel-NgModelChange', loadComponent: () => import('./components/main-data-binding/basic-ng-model-ng-model-change/basic-ng-model-ng-model-change').then((c) => c.BasicNgModelNgModelChange) }
+        ]
+    }
 ];
---
+++ src/app/components/main-data-binding/main-data-binding.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-data-binding',
+  imports: [RouterOutlet],
+  templateUrl: './main-data-binding.html',
+  styleUrl: './main-data-binding.css',
+})
+export class MainDataBinding {
+
+}
