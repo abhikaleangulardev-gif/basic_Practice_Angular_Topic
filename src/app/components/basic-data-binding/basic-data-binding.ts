c342756cf8f87cb099385dd60821b4d62dd58a9c
abhishek.kale <abhisahebpatil@gmail.com>
2026-03-04 11:59:17 +0530

    setup-basic_Practice_Angular_Topic

---
+++ src/app/components/basic-data-binding/basic-data-binding.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-data-binding',
+  imports: [],
+  templateUrl: './basic-data-binding.html',
+  styleUrl: './basic-data-binding.css',
+})
+export class BasicDataBinding {
+
+}
