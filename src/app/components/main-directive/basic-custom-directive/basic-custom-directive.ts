40f4f7a3308e9ddcf6288fcdd46bb3dc6b1e8586
abhishek.kale <abhisahebpatil@gmail.com>
2026-03-06 20:20:28 +0530

    completed-custom-directive

--- src/app/components/main-directive/basic-custom-directive/basic-custom-directive.ts
+++ src/app/components/main-directive/basic-custom-directive/basic-custom-directive.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { Focus } from '../../../custom/focus';
+import { Highligh } from '../../../custom/highligh';
 
 @Component({
   selector: 'app-basic-custom-directive',
-  imports: [],
+  imports: [CommonModule, Focus, Highligh],
   templateUrl: './basic-custom-directive.html',
   styleUrl: './basic-custom-directive.css',
 })
 export class BasicCustomDirective {
-
+  myParagraph: WritableSignal<string> = signal("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
 }
